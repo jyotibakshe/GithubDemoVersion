289c569dc326f4b5b94a7dfd03f6b300ba53430e
jyotibakshe <jyotipatelbakshe12@gmail.com>
2023-02-03 12:05:23 +0530

    hello Tl chckit

--- src/gitpackge/test2.java
+++ src/gitpackge/test2.java
@@ -4,6 +4,7 @@ public class test2 {
 
 	public static void main(String[] args) {
 			System.out.println("test2... by adding tester");
+			System.out.println("test2... by adding yessss tester");
 	}
 
 }
